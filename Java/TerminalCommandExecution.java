ece0fe28db2bb5773b0b1c5ee371af83af4875a1
Sathimantha Malalasekera <65760918+Cenfracee@users.noreply.github.com>
2020-11-01 16:02:46 +0530

    Create TerminalCommandExecution.java

---
+++ Java/TerminalCommandExecution.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TerminalTest {
+    public static void main(String[] args) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ls");
+        try {
+
+            Process process = processBuilder.start();
+
+            StringBuilder output = new StringBuilder();
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("Success!");
+                System.out.println(output);
+                System.exit(0);
+            } else {
+                //abnormal...
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
